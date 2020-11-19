1c053439c0bc8b8cfeedee9e9f7b419475fef1db
daniel.ni <erjian.nej@lazada.com>
2020-11-19 19:05:23 +0800

    Initial Commit

---
+++ Todalendar/TodalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodalendarApp.swift
+//  Todalendar
+//
+//  Created by UNDaniel on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct TodalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
